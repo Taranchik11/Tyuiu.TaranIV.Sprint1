74b46eb05ba83ccc55df4d2af3175cc7c39c5dbc
Taran72 <taranivan72@mail.ru>
2026-09-17 02:11:21 +0500

    Task0

---
+++ Tyuiu.TaranIV.Sprint1.Task1.V0.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TaranIV.Sprint1.Task1.V0.Lib
+{
+    public class DataService : ISprint1Task1V17
+    {
+        public double Calculate(double x, double y)
+        {
+            return x * y * (5 - x);
+        }
+    }
+}
