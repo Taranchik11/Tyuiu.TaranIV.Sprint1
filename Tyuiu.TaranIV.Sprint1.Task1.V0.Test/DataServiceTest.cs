ff8d47c437fed0322f3a91e90a88b44b553eb586
Taran72 <taranivan72@mail.ru>
2026-09-17 02:11:21 +0500

    Task1

---
+++ Tyuiu.TaranIV.Sprint1.Task1.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TaranIV.Sprint1.Task1.V0.Lib;
+
+namespace Tyuiu.TaranIV.Sprint1.Task1.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 7;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(42, res);
+
+        }
+    }
+}
--- Tyuiu.TaranIV.Sprint1.Task1.V0/Program.cs
+++ Tyuiu.TaranIV.Sprint1.Task1.V0/Program.cs
@@ -1,26 +1,35 @@
-﻿using Tyuiu.TaranIV.Sprint1.Task0.V0.Lib;
+﻿using Tyuiu.TaranIV.Sprint1.Task1.V0.Lib;
 
+double x;
+double y;
 DataService ds = new DataService();
 
 Console.Title = "Спринт #1 | Выполнил: Таран И. В. | ПКТб-26-1";
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* Спринт #1                                                               *");
-Console.WriteLine("* Тема: Базовые навыки работы в C#.                                       *");
-Console.WriteLine("* Задание #0                                                              *");
-Console.WriteLine("* Вариант #28                                                             *");
+Console.WriteLine("* Тема: Организация ввода\\вывода в консольных проектах.                  *");
+Console.WriteLine("* Задание #1                                                              *");
+Console.WriteLine("* Вариант #17                                                             *");
 Console.WriteLine("* Выполнил: Таран Иван Владимирович | ПКТб-26-1                           *");
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* УСЛОВИЕ:                                                                *");
-Console.WriteLine("* Написать программу, которая вычисляет выражение 48 / 12 - 48 / 6 / 4    *");
-Console.WriteLine("* и печатает результат на экране.                                         *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* вычисляет результат по формуле x * y * (5 - x) и печатает его на экране.*");
 Console.WriteLine("*                                                                         *");
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
 Console.WriteLine("***************************************************************************");
-Console.WriteLine("* 48 / 12 - 48 / 6 / 4                                                    *");
+
+Console.WriteLine("введите значение x:");
+x= Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("введите значение y:");
+y = Convert.ToDouble(Console.ReadLine());
+
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
 Console.WriteLine("***************************************************************************");
 
-Console.WriteLine(ds.Calculate());
+Console.WriteLine(ds.Calculate(x,y));
+
 Console.ReadLine();
