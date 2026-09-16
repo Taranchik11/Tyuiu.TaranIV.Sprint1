03131cb8702d977f798a6cfeff5984391ee08ce7
Taran72 <taranivan72@mail.ru>
2026-09-17 04:19:26 +0500

    Task2

---
+++ Tyuiu.TaranIV.Sprint1.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TaranIV.Sprint1.Task2.V21.Lib;
+
+namespace Tyuiu.TaranIV.Sprint1.Task2.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpession()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 7;
+            var res = ds.CalculateRectangleSquare(x,y);
+            Assert.AreEqual(21, res);
+        }
+    }
+}
--- Tyuiu.TaranIV.Sprint1.Task2.V21/Program.cs
+++ Tyuiu.TaranIV.Sprint1.Task2.V21/Program.cs
@@ -1,35 +1,34 @@
-﻿using Tyuiu.TaranIV.Sprint1.Task1.V0.Lib;
+﻿using Tyuiu.TaranIV.Sprint1.Task2.V21.Lib;
 
-double x;
-double y;
 DataService ds = new DataService();
+
+int x, y;
 
 Console.Title = "Спринт #1 | Выполнил: Таран И. В. | ПКТб-26-1";
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* Спринт #1                                                               *");
-Console.WriteLine("* Тема: Организация ввода\\вывода в консольных проектах.                  *");
-Console.WriteLine("* Задание #1                                                              *");
-Console.WriteLine("* Вариант #17                                                             *");
+Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+Console.WriteLine("* Задание #1.2                                                            *");
+Console.WriteLine("* Вариант #21                                                             *");
 Console.WriteLine("* Выполнил: Таран Иван Владимирович | ПКТб-26-1                           *");
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* УСЛОВИЕ:                                                                *");
-Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-Console.WriteLine("* вычисляет результат по формуле x * y * (5 - x) и печатает его на экране.*");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные  *");
+Console.WriteLine("* и вычисляет площадь прямоугольника.                                     *");
 Console.WriteLine("*                                                                         *");
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
 Console.WriteLine("***************************************************************************");
 
-Console.WriteLine("введите значение x:");
-x= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение x:");
+x = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("введите значение y:");
-y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение y:");
+y = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
 Console.WriteLine("***************************************************************************");
 
-Console.WriteLine(ds.Calculate(x,y));
+Console.WriteLine("Площадь прямоугольника равна:" + ds.CalculateRectangleSquare(x,y));
 
-Console.ReadLine();
