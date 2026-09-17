edb36afa9dfc6cfd4de2d1e6b82df27523e8dc2d
Taran72 <taranivan72@mail.ru>
2026-09-17 20:12:29 +0500

    Task3

---
+++ Tyuiu.TaranIV.Sprint1.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TaranIV.Sprint1.Task3.V12.Lib;
+
+namespace Tyuiu.TaranIV.Sprint1.Task3.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpession()
+        {
+            DataService ds = new DataService();
+            double x = 2.3;
+            double y = 2.5;
+            var wait = (x * y) / 2;
+            var res = ds.TriangleArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TaranIV.Sprint1.Task3.V12/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.TaranIV.Sprint1.Task3.V12.Lib;
+
+DataService ds = new DataService();
+double x, y;
+
+Console.Title = "Спринт #1 | Выполнил: Таран И. В. | ПКТб-26-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+Console.WriteLine("* Задание #1.3                                                            *");
+Console.WriteLine("* Вариант #12                                                             *");
+Console.WriteLine("* Выполнил: Таран Иван Владимирович | ПКТб-26-1                           *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные  *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите значение первого катета:");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение второго катета:");
+y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Площадь прямоугольного треугольника равна:" + ds.TriangleArea(x,y));
+Console.ReadKey();
