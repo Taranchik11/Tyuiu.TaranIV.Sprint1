f4745db6dbbc4d111331e12a2862378ece1ebab3
Taran72 <taranivan72@mail.ru>
2026-09-20 02:16:20 +0500

    Task5

---
+++ Tyuiu.TaranIV.Sprint1.Task5.V05.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TaranIV.Sprint1.Task5.V05.Lib;
+
+
+namespace Tyuiu.TaranIV.Sprint1.Task5.V05.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 32.521;
+            int d = ds.Calculate(x);
+            int wait = 5;
+            Assert.AreEqual(wait, d);
+        }
+    }
+}
